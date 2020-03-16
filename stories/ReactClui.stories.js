af52292caa148a27050294926c0ba04007078f05
Zach Sherman <zach@anemon.es>
2020-03-16 15:10:56 -0700

    add echo demo

--- stories/ReactClui.stories.js
+++ stories/ReactClui.stories.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Terminal from 'Terminal'
 
 storiesOf('Terminal', module).add('render terminal', () => (
   <Terminal inputStr={'initial val'} />
+))
+storiesOf('Terminal', module).add('echo command', () => (
+  <Terminal inputStr={'echo "Bernie 2020"'} />
 ))
